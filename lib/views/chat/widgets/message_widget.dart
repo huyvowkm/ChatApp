9268c8cc41135760020ff05a90ee55fb1f36ea70
HuyVoWakumo <huyvo@wakumo.vn>
2024-07-30 17:29:52 +0700

    feat: scroll to end, make message easier to read

--- lib/views/chat/widgets/message_widget.dart
+++ lib/views/chat/widgets/message_widget.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/models/message_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MessageWidget extends StatelessWidget {
   MessageModel message;
@@ -8,18 +9,44 @@ class MessageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isSentByCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Visibility(
+          visible: !isSentByCurrentUser,
+          child: _avatar()
+        ),
+        _messageBox(context),
+      ],
+    );
+  }
+
+  Widget _messageBox(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      
+      constraints: BoxConstraints(maxWidth: 0.7 * screenWidth),
       padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.only(top: 5, bottom: 5),
+      margin: const EdgeInsets.only(top: 3, bottom: 3),
       decoration: BoxDecoration(
         color: isSentByCurrentUser ? Colors.blue : Colors.grey[700],
-        borderRadius: BorderRadius.circular(15)
+        borderRadius: BorderRadius.circular(20)
       ),
       child: Text(
-        '${message.from.name}: ${message.content}', 
-        style: const TextStyle(fontSize: 16, color: Colors.white)
+        message.content, 
+        maxLines: null,
+        // overflow: TextOverflow.ellipsis,
+        style: const TextStyle(fontSize: 18, color: Colors.white),
       )
+    );
+  }
+
+  Widget _avatar() {
+    return  Container(
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+      ),
+      child: FlutterLogo(size: 30)
     );
   }
 }
